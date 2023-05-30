6341c2e66d5c11b2156a5e58639595fbcc3e29dd
Vitor Balbo <101026113+balb-0@users.noreply.github.com>
2023-05-29 22:29:11 -0300

    att

--- script.js
+++ script.js
@@ -39,3 +39,58 @@ fecharSeo.addEventListener('click', () => {
     body.classList.remove('block-scroll')
 })
 
+
+const lista = window.document.querySelector(".lista")
+const toggle = window.document.querySelector(".toggle")
+const home = window.document.querySelector('.home_close')
+const sobre = window.document.querySelector('.sobre_close')
+const servicos = window.document.querySelector('.servicos_close')
+
+
+
+toggle.addEventListener('click', () => {
+    const visivel = lista.getAttribute('data-visible')
+    
+    if (visivel === "false") {
+        lista.setAttribute('data-visible', true)
+        toggle.setAttribute('aria-expanded', true)
+    } else {
+        lista.setAttribute('data-visible', false)
+        toggle.setAttribute('aria-expanded', false)
+        
+    }
+})
+
+home.addEventListener('click', () => {
+    lista.setAttribute('data-visible', false)
+    toggle.setAttribute('aria-expanded', false)
+})
+
+sobre.addEventListener('click', () => {
+    lista.setAttribute('data-visible', false)
+    toggle.setAttribute('aria-expanded', false)
+})
+
+servicos.addEventListener('click', () => {
+    lista.setAttribute('data-visible', false)
+    toggle.setAttribute('aria-expanded', false)
+})
+
+const menuItems = document.querySelectorAll('.navega a[href^="#"]')
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scroll);
+})
+ 
+function scroll(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+    
+    window.scrollTo({
+        left: 0,
+        top: to - 114,
+        behavior: "smooth"
+    });
+}
